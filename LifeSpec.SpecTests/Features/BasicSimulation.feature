15637f9ab3b5d4bc79b1fb7b5601552a4ecce2cf
Josh Elster <josh@liquidelectron.com>
2012-06-12 23:12:50 -0500

    Basic feature defined
    * Install-package SpecFlow -p LifeSpec.SpecTests
    * Change default test runner from nUnit to MSTest in app.config
    * Step bindings stubbed out and left pending

---
+++ LifeSpec.SpecTests/Features/BasicSimulation.feature
@@ -0,0 +1,12 @@
+﻿Feature: Basic Simulation
+	In order to explore the principles of computational science
+	As a dummy
+	I want to run a simulation of Conway's Game Of Life
+	so that I can stop being a dummy
+
+
+Scenario: Live cell dies 
+	Given A grid 60 wide by 60 high
+	And a live cell at x15 y30
+	When the simulation is advanced 1 step
+	Then the cell at x15 y30 should be dead
---
+++ LifeSpec.SpecTests/Steps/LifeSteps.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace LifeSpec.SpecTests.Steps
+{
+    [Binding]
+    public class LifeSteps
+    {
+        [Given(@"A grid (\d+) wide by (\d+) high")]
+        public void Given_a_grid(int width, int height)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"a live cell at x(\d+) y(\d+)")]
+        public void Given_a_cell(int xPos, int yPos)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"the simulation is advanced (\d+) step")]
+        public void When_the_simulation_is_advanced(int stepsToAdvance)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the cell at x(\d+) y(\d+) should be dead")]
+        public void Then_the_cell_should_be_dead(int xPos, int yPos)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
